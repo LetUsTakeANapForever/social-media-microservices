580b070dcf8edae4a1e1a79cc6b23593a2c61c64
LetUsTakeANapForever <kandamkankaw@gmail.com>
2026-02-26 15:19:42 +0700

    fix(rabbitmq): handle startup timing issue with connection retry loop

--- search-service/src/utils/rabbitmq.js
+++ search-service/src/utils/rabbitmq.js
@@ -7,16 +7,22 @@ let channel = null;
 const EXCHANGE_NAME = "social_media_events";
 
 async function connectToRabbitMQ() {
-    try {
-        connection = await amqp.connect(process.env.RABBITMQ_URL);
-        channel = await connection.createChannel();
+    const url = process.env.RABBITMQ_URL;
 
-        await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
-        logger.info("Connected to rabbit mq");
-        return channel;
-    } catch (e) {
-        logger.error("Error connecting to rabbit mq", e);
+    while (!channel) {
+        try {
+            connection = await amqp.connect(url);
+            channel = await connection.createChannel();
+            await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+
+            logger.info("Connected to RabbitMQ");
+        } catch (err) {
+            logger.warn("RabbitMQ not ready, retrying in 5s...");
+            await new Promise(res => setTimeout(res, 5000));
+        }
     }
+
+    return channel;
 }
 
 async function consumeEvent(routingKey, callback) {
